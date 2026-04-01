8653ae4eb4d9b09df9e16cdb908f23c33b6c79ba
Arthur Silveira <designbyaarthur@gmail.com>
2026-03-31 23:10:52 -0300

    Fixed prisma using accelerateUrl

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,8 +1,6 @@
 import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-  accelerateUrl: process.env.DATABASE_URL
-});
+const prisma = new PrismaClient();
 
 export default prisma;
